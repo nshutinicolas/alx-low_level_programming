6327fcf8c8938f46309b2c7745c2fd9949d5ece7
nshutinicolas <nshuti.nicolas@yahoo.com>
2021-02-03 17:54:27 +0000

    0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  char c;
+  int i;
+  long l;
+  float f;
+  long long ll;
+  printf("Size of a char: %ld byte(s)\n",sizeof(c));
+  printf("Size of an int: %ld byte(s)\n",sizeof(i));
+  printf("Size of a long int: %ld byte(s)\n",sizeof(l));
+  printf("Size of a long long int: %ld byte(s)\n",sizeof(ll));
+  printf("Size of a float: %ld byte(s)\n",sizeof(f));
+  return (0);
+}
