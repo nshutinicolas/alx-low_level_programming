cb114721e697804cdac2c199eac478805945c06b
nshutinicolas <nshuti.nicolas@yahoo.com>
2021-02-04 20:47:18 +0000

    print  combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* print combination of numbers */
+
+int main(void)
+{
+	int first, last;
+	for (first = '0';first <= '9'; first++) 
+	{
+		for (last = first + 1; last <='9'; last++)
+		{
+			putchar(first);
+			putchar(last);
+			if(last <= '9'){
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
