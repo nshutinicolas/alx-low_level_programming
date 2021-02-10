86f9d4d1f2e1745bb624a1deb70fe8b915203136
nshutinicolas <nshuti.nicolas@yahoo.com>
2021-02-10 18:00:45 +0000

    0x03-debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 	/*
 	 *while (i < 10)
 	 *{
 	 *	putchar(i);
 	 *}
 	 */
-
 	printf("Infinite loop a voided! \\o/\n");
 
 	return (0);
