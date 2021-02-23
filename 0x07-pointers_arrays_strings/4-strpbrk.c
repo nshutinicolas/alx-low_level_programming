b85893dbc94d376b6130ec7518f2d0f9487c2f81
nshutinicolas <nshuti.nicolas@yahoo.com>
2021-02-23 10:00:22 +0000

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -19,7 +19,10 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (&s[i]);
+			{
+				s = &s[i];
+				return (s);
+			}
 		}
 	}
 	return (NULL);
