2054aba86a5fb756e5717dc30ed63662977929dd
nshutinicolas <nshuti.nicolas@yahoo.com>
2021-02-23 11:44:02 +0000

    0x07-pointers_arrays_printes

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - functions that printts the sum of diagonals
+ * @a: array for integers
+ * @size: size of the array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		j = (i * size) + i;
+		sum1 += a[j];
+	}
+	for (i = 0; i < size; i++)
+	{
+		j = (i * size) - i;
+		sum2 += a[j];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
