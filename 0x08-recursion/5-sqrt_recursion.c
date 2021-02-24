08fb8cebe856be8e76044fdaa851d2708f20a951
nshutinicolas <nshuti.nicolas@yahoo.com>
2021-02-24 20:59:44 +0000

    final

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * root - helper fctn
+ * @n: number
+ * Return: root
+ */
+
+int root(int n, itn i)
+{
+	if (n == (i * i))
+		return (i);
+}
+
+/**
+ * _sqrt_recursion - function to determine the sqrt
+ * @n: number
+ * Return: returns sqrt of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (root(n, 1));
+}
